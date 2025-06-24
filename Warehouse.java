9941a38b15cae4143b3e1d30428d1c6b4f4a91e0
Jonathan07200 <jonathantreerhapat.rep@student.mahidol.edu>
2025-06-24 16:42:40 +0000

    fixed wrong getters naming

--- Warehouse.java
+++ Warehouse.java
@@ -1,15 +1,21 @@
 package Project2_6680081;
 
 public class Warehouse {
+    //private final int id;
     private final String name;
     private int balance = 0;
+    private int totalAdded = 0;
     public Warehouse (String name)               { this.name = name; }
-    public synchronized void put (int amount)    { balance += amount; }
-    public synchronized int get (int amount) {
+    public synchronized void put (int amount)    { balance += amount; 
+        totalAdded += amount;}
+    public synchronized int getMaterials (int amount) {
         int taken = Math.min (amount, balance);
         balance -= taken;
         return taken;
     }
-    public synchronized int getBalance()         { return balance; } 
+    public synchronized int getMaterialCount()         { return balance; } 
+    public synchronized int getTotalAdded()      { return totalAdded; }
     public String getName()                      { return name; } 
+    //public int getId()                           { return id; }
+    
 }
