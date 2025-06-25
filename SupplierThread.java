71b69d82a7e77f469d45954eaeb94bd59101f075
boonluar <167950561+boonluar@users.noreply.github.com>
2025-06-25 10:14:44 +0700

    Update SupplierThread.java

--- SupplierThread.java
+++ SupplierThread.java
@@ -3,26 +3,23 @@ package Project2_6680081;
 import java.util.*;
 import java.io.*;
 
-/*  for (int i = 0; i < supplierNum; i++) {
-            Thread supplier = new Thread(new SupplierThread(
-                warehouses, supplierMin, supplierMax, days, coordinator
-            ), "Supplier_" + (i + 1));
-            allThreads.add(supplier);
-            supplier.start();
-        }
-
+/*  
+    for (int i = 0; i < supplierNum; i++) {
+        Thread supplier = new Thread(new SupplierThread(
+            warehouses, supplierMin, supplierMax, days, coordinator
+        ), "Supplier_" + (i + 1));
+        allThreads.add(supplier);
+        supplier.start();
+    }
 */
 
-
-
 public class SupplierThread extends Thread {
-    private final List<Warehouse> warehouses;
+    private final List<Warehouse> warehouses; 
     private final int supplierMin;
     private final int supplierMax;
     private final int totalDays;
     private final SimulationCoordinator coordinator;
     private final Random random;
-
 
     public SupplierThread(List<Warehouse> warehouses, int supplierMin, int supplierMax, int days, SimulationCoordinator coordinator) { 
         this.warehouses = warehouses;
@@ -37,16 +34,25 @@ public class SupplierThread extends Thread {
     public void run() {
         if (coordinator == null) {
             throw new IllegalArgumentException("Coordinator must not be null");
-            }
+        }
+
         for (int day = 0; day < totalDays; day++) {
             try {
-                coordinator.startDayBarrier.await();
+                coordinator.dailyStartBarrier.await();
+
                 int amount = supplierMin + random.nextInt(supplierMax - supplierMin + 1);
-                int warehouseNumber = random.nextInt(warehouses.size()); //random warehouse, no repeat of same warehouse
-                warehouses.get(warehouseNumber).put(amount); //select the warehouse, add the amount
-                //System.out.println(getName() + "  >>  put " + amount + " materials    Warehouse_" + warehouseNumber + " balance = %-5d",); //unused output, missing getTotal amount
-                coordinator.suppliersDoneBarrier.await();
-                coordinator.productionDoneBarrier.await();
+                int warehouseNumber = random.nextInt(warehouses.size());
+                warehouses.get(warehouseNumber).put(amount);
+
+                
+                System.out.printf("%s  >>  put %d materials    Warehouse_%d balance = %-5d\n",
+                    Thread.currentThread().getName(),
+                    amount,
+                    warehouseNumber,
+                    warehouses.get(warehouseNumber).getMaterialCount());
+
+                coordinator.supplierDoneBarrier.await();
+                coordinator.factoryDoneBarrier.await();
 
             } catch (Exception e) {
                 System.out.println(getName() + ">> Error type: " + e.getMessage());
